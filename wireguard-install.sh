5fbbc91771737444472f63b72a98138ea40b7bad
PyBorov <80942910+PyBorov@users.noreply.github.com>
2024-05-31 10:20:23 +0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -40,7 +40,7 @@ function checkOS() {
 			exit 1
 		fi
 		OS=debian # overwrite if raspbian
-	elif [[ ${OS} == "ubuntu"]]; then
+	elif [[ ${OS} == "ubuntu" ]]; then
 		RELEASE_YEAR=$(echo "${VERSION_ID}" | cut -d'.' -f1)
 		if [[ ${RELEASE_YEAR} -lt 18 ]]; then
 			echo "Your version of Ubuntu (${VERSION_ID}) is not supported. Please use Ubuntu 18.04 or later"
